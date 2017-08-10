fb22115153f54f21615d7e758d5cf4e430e9edbc
Daniel Zahoranszky <zahoranszky.d.milan@gmail.com>
2017-08-10 15:34:11 +0200

    basic ui completed

--- MediaLocator/MediaLocator/MainWindow.xaml.cs
+++ MediaLocator/MediaLocator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace meLo
 {
@@ -20,31 +22,67 @@ namespace meLo
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += new EventHandler(timer_Tick);
         }
 
         private void btnPlay1_Click(object sender, RoutedEventArgs e)
         {
+            mediaPlayer.Play();
+        }
 
+        private void btnStop1_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+
+        private void btnPause1_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Pause();
+        }
+
+        private void btnFolder1_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd;
+            ofd = new OpenFileDialog();
+            ofd.AddExtension = true;
+            ofd.DefaultExt = "*.*";
+            ofd.Filter = "Media Files (*.*)|*.*";
+            ofd.ShowDialog();
+
+            try { mediaPlayer.Source = new Uri(ofd.FileName); }
+            catch { new NullReferenceException("error"); }
+
+            System.Windows.Threading.DispatcherTimer dispatchertimer = new System.Windows.Threading.DispatcherTimer();
+            dispatchertimer.Tick += new EventHandler(timer_Tick);
+            dispatchertimer.Interval = new TimeSpan(0, 0, 1);
+            dispatchertimer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            mediaSlider.Value = mediaPlayer.Position.TotalSeconds;
+        }
+        
+        private void mediaSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            mediaPlayer.Position = TimeSpan.FromSeconds(mediaSlider.Value);
+        }
+
+        private void slider_vol_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            mediaPlayer.Volume = (double)Volume.Value;
+        }
+
+        private void mediaPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            TimeSpan ts = mediaPlayer.NaturalDuration.TimeSpan;
+            mediaSlider.Maximum = ts.TotalSeconds;
+            timer.Start();
         }
     }
 }
