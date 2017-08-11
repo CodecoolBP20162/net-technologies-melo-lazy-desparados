fad492ec5d2a7451f04939de5e0766351a0e1cb7
Amador99 <abelofvarad@gmail.com>
2017-08-11 12:19:19 +0200

    Fixed duplications caused by merge

--- MediaLocator/MediaLocator/MainWindow.xaml.cs
+++ MediaLocator/MediaLocator/MainWindow.xaml.cs
@@ -147,48 +147,6 @@ namespace meLo
             mediaPlayer.Position = TimeSpan.FromSeconds(mediaSlider.Value);
         }
 
-        private void btnPlay1_Click(object sender, RoutedEventArgs e)
-        {
-            mediaPlayer.Play();
-        }
-
-        private void btnStop1_Click(object sender, RoutedEventArgs e)
-        {
-            mediaPlayer.Stop();
-        }
-
-        private void btnPause1_Click(object sender, RoutedEventArgs e)
-        {
-            mediaPlayer.Pause();
-        }
-
-        private void btnFolder1_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog ofd;
-            ofd = new OpenFileDialog();
-            ofd.AddExtension = true;
-            ofd.DefaultExt = "*.*";
-            ofd.Filter = "Media Files (*.*)|*.*";
-            ofd.ShowDialog();
-
-            try { mediaPlayer.Source = new Uri(ofd.FileName); }
-            catch { new NullReferenceException("error"); }
-
-            System.Windows.Threading.DispatcherTimer dispatchertimer = new System.Windows.Threading.DispatcherTimer();
-            dispatchertimer.Tick += new EventHandler(timer_Tick);
-            dispatchertimer.Interval = new TimeSpan(0, 0, 1);
-            dispatchertimer.Start();
-        }
-        void timer_Tick(object sender, EventArgs e)
-        {
-            mediaSlider.Value = mediaPlayer.Position.TotalSeconds;
-        }
-        
-        private void mediaSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            mediaPlayer.Position = TimeSpan.FromSeconds(mediaSlider.Value);
-        }
-
         private void slider_vol_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             mediaPlayer.Volume = (double)Volume.Value;
