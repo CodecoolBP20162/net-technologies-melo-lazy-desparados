1a6a6a677e9067b4d972fbbcfd074fd63684ec61
Daniel Zahoranszky <zahoranszky.d.milan@gmail.com>
2017-08-18 14:20:33 +0200

    Added some design changes , shadow effect , new button colors and background theme

--- MediaLocator/MediaLocator/MainWindow.xaml.cs
+++ MediaLocator/MediaLocator/MainWindow.xaml.cs
@@ -75,54 +75,7 @@ namespace meLo
         }
 
 
-        /*media part*/
-        private static List<AudioSplitOutput> SplitMp3File(string baseNameForSplits, string sourceFileName, string destinationPath)
-        {
-            List<AudioSplitOutput> outputAudioSplitList = new List<AudioSplitOutput>();
-
-            int splitLength = 480; // seconds
-
-            int secsOffset = 0;
-            int splitIndex = 0;
-
-            using (var reader = new Mp3FileReader(sourceFileName))
-            {
-
-                FileStream writer = null;
-                Action createWriter = new Action(() => {
-                    string newBaseNameForSplit = baseNameForSplits + "-" + (++splitIndex).ToString();
-                    string newFileName = System.IO.Path.Combine(destinationPath, newBaseNameForSplit + ".mp3");
-                    if (File.Exists(newFileName))
-                    {
-                        File.Delete(newFileName);
-                    }
-                    writer = File.Create(newFileName);
-
-                    AudioSplitOutput audioSplitOutput = new AudioSplitOutput();
-                    audioSplitOutput.FileName = newFileName;
-                    audioSplitOutput.AudioTimeOffsetTotalMilliseconds = ulong.Parse(reader.CurrentTime.TotalMilliseconds.ToString());
-                    outputAudioSplitList.Add(audioSplitOutput);
-                });
-
-                Mp3Frame frame;
-                while ((frame = reader.ReadNextFrame()) != null)
-                {
-                    if (writer == null) createWriter();
-
-                    if ((int)reader.CurrentTime.TotalSeconds - secsOffset >= splitLength)
-                    {
-                        // time for a new file
-                        writer.Dispose();
-                        createWriter();
-                        secsOffset = (int)reader.CurrentTime.TotalSeconds;
-                    }
-
-                    writer.Write(frame.RawData, 0, frame.RawData.Length);
-                }
-                if (writer != null) writer.Dispose();
-            }
-            return outputAudioSplitList;
-        }
+        
 
 
         private void btnPlay1_Click(object sender, RoutedEventArgs e)
